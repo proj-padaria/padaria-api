80ff2047a56713d512d80207206bd261f6bfccea
Gustavo <meiragustavo@hotmail.com>
2023-09-04 21:52:02 -0300

    Adicionado arquivos Java Spring Boot

---
+++ src/test/java/br/com/gar/padaria/PadariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gar.padaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

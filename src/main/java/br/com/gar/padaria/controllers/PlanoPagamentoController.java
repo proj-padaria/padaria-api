fd6ee5c0518d96510e5d4806e8f516f08eaba8af
Gustavo <meiragustavo@hotmail.com>
2023-09-12 15:41:57 -0300

    Criado Save e Delete de PlanoPagamento

--- src/main/java/br/com/gar/padaria/controllers/PlanoPagamentoController.java
+++ src/main/java/br/com/gar/padaria/controllers/PlanoPagamentoController.java
@@ -2,10 +2,7 @@ package br.com.gar.padaria.controllers;
 import br.com.gar.padaria.models.PlanoPagamento;
 import br.com.gar.padaria.repositories.PlanoPagamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +21,16 @@ public class PlanoPagamentoController {
     @GetMapping("id/{id}")
     public PlanoPagamento selectById(@PathVariable("id") Integer id) {
         return planosPagamentoRepository.findById(id).get();
+    }
+
+    @DeleteMapping("delete/{id}")
+    public void planoPagamentoDeleteById (@PathVariable Integer id){
+        planosPagamentoRepository.deleteById(id);
+    }
+
+    @PostMapping("save")
+    public PlanoPagamento planoPagamentoSave (@RequestBody PlanoPagamento planoPagamentoModel){ // oq é isso plano pagamento model?
+        PlanoPagamento planoPagamento = planosPagamentoRepository.save(planoPagamentoModel);
+        return planoPagamento;
     }
 }

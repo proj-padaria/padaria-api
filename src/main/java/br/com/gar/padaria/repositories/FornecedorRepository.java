253ba70962ad18532b7c14161d6c9dce442c6238
Renan Meira <meirarenan@hotmail.com>
2023-09-28 19:19:23 -0300

    Adicionado Fornecedores Repository

--- src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
+++ src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
@@ -1,12 +1,9 @@
 package br.com.gar.padaria.repositories;
 import br.com.gar.padaria.models.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
 
     List<Fornecedor> findAll();
-
 }

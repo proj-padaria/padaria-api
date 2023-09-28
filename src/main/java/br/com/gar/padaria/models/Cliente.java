f859e4a083ce415f342f39748fb4fbc61ec52ca5
Renan Meira <meirarenan@hotmail.com>
2023-09-28 19:15:34 -0300

    Adicionado o modelo de Cliente

--- src/main/java/br/com/gar/padaria/models/Cliente.java
+++ src/main/java/br/com/gar/padaria/models/Cliente.java
@@ -9,31 +9,30 @@ import java.util.Date;
     @Table(name = "clientes")
 
     public class Cliente implements Serializable {
-        private static final long serialVersion = 1L;
+    private static final long serialVersion = 1L;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-        @OneToOne
-        @JoinColumn(name = "pessoa_id", foreignKey = @ForeignKey(name = "pessoa_id"))
-        private Pessoa pessoa;
+    @OneToOne
+    @JoinColumn(name = "pessoa_id", foreignKey = @ForeignKey(name = "pessoa_id"))
+    private Pessoa pessoa;
 
-        @Column(nullable = false)
-        private Date data_nascimento;
+    @Column(nullable = false)
+    private Date data_nascimento;
 
-        @Column(length = 14)
-        private String  renda_mensal;
+    @Column(length = 14)
+    private String renda_mensal;
 
-        @Column(nullable = false, length = 20)
-        private String documento;
+    @Column(nullable = false, length = 20)
+    private String documento;
 
-        @Column(length = 14)
-        private String  limite_de_credito;
+    @Column(length = 14)
+    private String limite_de_credito;
 
-        @Column(nullable = false)
-        private Integer dia_vencimento_fiado;
-       }
-
+    @Column(nullable = false)
+    private Integer dia_vencimento_fiado;
+    }
 
 

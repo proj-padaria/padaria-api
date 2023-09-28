56a515f4a60a6c56a0f14ca29ba268413f0cea43
Renan Meira <meirarenan@hotmail.com>
2023-09-28 20:08:02 -0300

    Adicionad Delete ao Vendas Controller

--- src/main/java/br/com/gar/padaria/controllers/VendasController.java
+++ src/main/java/br/com/gar/padaria/controllers/VendasController.java
@@ -3,10 +3,7 @@ package br.com.gar.padaria.controllers;
 import br.com.gar.padaria.models.Vendas;
 import br.com.gar.padaria.repositories.VendasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +26,8 @@ public class VendasController {
         return vendasRepository.findById(id).get();
     }
 
+    @DeleteMapping(value = "delete/{id}")
+    public void clienteDeleteById(@PathVariable Integer id) {
+        vendasRepository.deleteById(id);
+    }
 }

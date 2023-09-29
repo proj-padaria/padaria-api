fd88df81ae007f36e4c94a52099c495464bc8054
Renan Meira <meirarenan@hotmail.com>
2023-09-29 16:21:01 -0300

    Adicionada query que realiza a consulta dos produtos com margem menor

--- src/main/java/br/com/gar/padaria/repositories/ProdutoRepository.java
+++ src/main/java/br/com/gar/padaria/repositories/ProdutoRepository.java
@@ -28,6 +28,7 @@ public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
 
     List<VerificaPontoPedidoDTO> verifica_ponto_pedido();
 
+
     @Query(value = "SELECT departamento_id, produto_id, nome, preco_venda, preco_unitario " +
             "       FROM produtos " +
             "       INNER JOIN compras_itens" +
@@ -36,4 +37,5 @@ public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
             "       ORDER BY nome ", nativeQuery = true)
 
     List<ProdutosMargemMenorDTO> produtos_margem_menor();
+
     }

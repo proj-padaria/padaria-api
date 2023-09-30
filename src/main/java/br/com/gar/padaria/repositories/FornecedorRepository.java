e59462f1bba68a4887f86226190d653661aa0603
Renan Meira <meirarenan@hotmail.com>
2023-09-29 21:37:22 -0300

    Adicionado p.nome ao Fornecedor Repository

--- src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
+++ src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
@@ -11,14 +11,14 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>
     List<Fornecedor> findAll();
 
 
-    @Query(value =  "SELECT c.fornecedor_id, SUM (c.valor_total_recebido) AS valor_total_no_periodo " +
+    @Query(value =  "SELECT c.fornecedor_id, p.nome, SUM (c.valor_total_recebido) AS valor_total_no_periodo " +
                     "FROM compras c " +
                     "	INNER JOIN fornecedores f " +
                     "		ON f.id = c.fornecedor_id " +
                     "	INNER JOIN pessoas p " +
                     "		ON p.id = f.pessoa_id " +
                     "WHERE c.data_recebimento BETWEEN '01/09/2023' AND '30/09/2023' " +
-                    "GROUP BY c.fornecedor_id " +
+                    "GROUP BY c.fornecedor_id, p.nome " +
                     "ORDER BY valor_total_no_periodo DESC ", nativeQuery = true)
 
     List<FornecedorMaiorGastoDTO> fornecedor_maior_gasto();

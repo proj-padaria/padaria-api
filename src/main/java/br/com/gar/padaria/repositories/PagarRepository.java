33edaa03dd1c923d62085170041ae4164010725d
Renan Meira <meirarenan@hotmail.com>
2023-10-01 19:32:30 -0300

    Adicionado o Pagar Repository

---
+++ src/main/java/br/com/gar/padaria/repositories/PagarRepository.java
@@ -0,0 +1,12 @@
+package br.com.gar.padaria.repositories;
+
+import br.com.gar.padaria.models.Pagar;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PagarRepository extends JpaRepository<Pagar,Integer> {
+
+
+
+
+
+}

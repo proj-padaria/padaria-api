ed2e1eb92d993784f8e7c0301fb99410e4ad1f04
Renan Meira <meirarenan@hotmail.com>
2023-10-01 19:30:54 -0300

    Completada a tabela de pagar

--- src/main/java/br/com/gar/padaria/models/Pagar.java
+++ src/main/java/br/com/gar/padaria/models/Pagar.java
@@ -1,4 +1,38 @@
 package br.com.gar.padaria.models;
 
-public class Pagar {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "pagar")
+
+public class Pagar implements Serializable {
+    private static final long serialVersion = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "fornecedor_id")
+    private Fornecedor fornecedor;
+
+    @ManyToOne
+    @JoinColumn(name = "compra_id")
+    private Compra compra;
+
+    @Column(nullable = false)
+    private Double valor;
+
+    @Column(nullable = true, length = 8)
+    private String data_vencimento;
+
+    @Column(nullable = true, length = 8)
+    private String data_pagamento;
+
+    @Column(nullable = false)
+    private Double valor_pago;
 }

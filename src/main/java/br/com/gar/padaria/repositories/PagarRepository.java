8598822ae8ed0b950dab8d6d969f7d996e961a53
Renan Meira <meirarenan@hotmail.com>
2023-10-01 21:10:35 -0300

    Arrumada identação da Query no Pagar Repository

--- src/main/java/br/com/gar/padaria/repositories/PagarRepository.java
+++ src/main/java/br/com/gar/padaria/repositories/PagarRepository.java
@@ -9,14 +9,14 @@ import java.util.List;
 public interface PagarRepository extends JpaRepository<Pagar,Integer> {
 
     @Query(value =  "SELECT pg.fornecedor_id, pg.compra_id, p.nome, pg.data_vencimento, SUM (pg.valor - pg.valor_pago) AS valor_a_pagar " +
-            "FROM pagar pg " +
-            "	INNER JOIN fornecedores f " +
-            "		ON pg.fornecedor_id = f.id " +
-            "	INNER JOIN pessoas p " +
-            "		ON f.pessoa_id = p.id " +
-            "WHERE pg.data_vencimento <= current_date " +
-            "GROUP BY pg.fornecedor_id, pg.compra_id,p.nome, pg.data_vencimento, pg.valor " +
-            "ORDER BY data_vencimento " , nativeQuery = true)
+                    "FROM pagar pg " +
+                    "	INNER JOIN fornecedores f " +
+                    "		ON pg.fornecedor_id = f.id " +
+                    "	INNER JOIN pessoas p " +
+                    "		ON f.pessoa_id = p.id " +
+                    "WHERE pg.data_vencimento <= current_date " +
+                    "GROUP BY pg.fornecedor_id, pg.compra_id,p.nome, pg.data_vencimento, pg.valor " +
+                    "ORDER BY data_vencimento " , nativeQuery = true)
 
     List<TitulosAPagarDTO> titulos_a_pagar_ate_hoje();
 }

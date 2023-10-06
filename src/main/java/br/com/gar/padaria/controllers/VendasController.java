e72d4378d147458777774829d1de1eafe738090a
anaju0113 <julinha.ajsp@gmail.com>
2023-10-06 12:31:19 -0300

    Criação do endpoint de cliente_maior_valor_compra

--- src/main/java/br/com/gar/padaria/controllers/VendasController.java
+++ src/main/java/br/com/gar/padaria/controllers/VendasController.java
@@ -5,8 +5,13 @@ import br.com.gar.padaria.models.Cliente;
 import br.com.gar.padaria.models.Vendas;
 import br.com.gar.padaria.repositories.VendasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -38,8 +43,34 @@ public class VendasController {
         vendasRepository.deleteById(id);
     }
 
-    @GetMapping("clientesMaiorCompraIntervalo")
-    public List<ClientesMaiorCompraIntervaloDTO> clientes_maior_compra_intervalo() {
-        return vendasRepository.clientes_maior_compra_intervalo();
+    @GetMapping("clientesMaiorCompraIntervalo/{dataInicial}/{dataFinal}")
+    public List<ClientesMaiorCompraIntervaloDTO> cliente_maior_valor_compra(@PathVariable Date dataInicial, @PathVariable Date dataFinal) {
+        return vendasRepository.cliente_maior_valor_compra(dataInicial, dataFinal);
     }
+
+
+
+   /* @GetMapping("clientesMaiorCompraIntervalo/{di}/{df}")
+    public List<ClientesMaiorCompraIntervaloDTO> cliente_maior_valor_compra(
+            @RequestParam("dataInicial") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataInicial,
+            @RequestParam("dataFinal") @DateTimeFormat(pattern = "yyyy-MM-dd") Date dataFinal) {
+        // Faça o processamento com as datas aqui
+        return vendasRepository.cliente_maior_valor_compra(dataInicial, dataFinal);
+
+}*/
+
+    @GetMapping("clientesMaiorCompraIntervalo/{di}/{df}")
+    public List<ClientesMaiorCompraIntervaloDTO> cliente_maior_valor_compra(@PathVariable @DateTimeFormat String di, @PathVariable @DateTimeFormat String df) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date dataInicial = dateFormat.parse(di);
+            Date dataFinal = dateFormat.parse(df);
+            // Use parsedDate in your code
+        } catch (ParseException e) {
+            // Handle the parsing exception
+        }
+
+        return null;
+    }
+
 }

a3b3296945eed5ef300c1d6c614daa4e5c8fc4ab
Renan Meira <meirarenan@hotmail.com>
2023-10-06 14:59:13 -0300

    Arrumado Fornecedor Controller para usar Parametros

--- src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
+++ src/main/java/br/com/gar/padaria/repositories/FornecedorRepository.java
@@ -4,6 +4,7 @@ import br.com.gar.padaria.models.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
@@ -17,11 +18,11 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>
                     "		ON f.id = c.fornecedor_id " +
                     "	INNER JOIN pessoas p " +
                     "		ON p.id = f.pessoa_id " +
-                    "WHERE c.data_recebimento BETWEEN '01/09/2023' AND '30/09/2023' " +
+                    "WHERE c.data_recebimento BETWEEN ?1 AND ?2 " +
                     "GROUP BY c.fornecedor_id, p.nome " +
                     "ORDER BY valor_total_no_periodo DESC ", nativeQuery = true)
 
-    List<FornecedorMaiorGastoDTO> fornecedor_maior_gasto();
+    List<FornecedorMaiorGastoDTO> fornecedor_maior_gasto(LocalDate dataInicial, LocalDate dataFinal);
 }
 
 
